7856e8d776d5deddba04b6fcd82b43aa0ed7ca25
AlexandroMtzG <alex.martinez@absys.com.mx>
2024-06-26 16:00:46 -0600

    session: secure cookie with JWT

--- lib/session.ts
+++ lib/session.ts
@@ -1,9 +1,10 @@
 import { parse, serialize } from "cookie";
 import { cookies } from "next/headers";
 import { defaultThemeColor, defaultThemeScheme } from "./theme";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 const SESSION_COOKIE_NAME = "RS_session";
-const SESSION_SECRET = process.env.SESSION_SECRET;
+const SESSION_SECRET = process.env.SESSION_SECRET || "";
 
 if (!SESSION_SECRET) {
   throw new Error("SESSION_SECRET must be set");
@@ -15,15 +16,28 @@ export type UserSession = {
   theme?: string;
 };
 
-function getUserSession() {
+function getUserSession(): UserSession | null {
   const cookieStore = cookies();
   const sessionCookie = cookieStore.get(SESSION_COOKIE_NAME)?.value;
   if (!sessionCookie) return null;
+
   try {
-    const parsed = parse(sessionCookie);
-    return JSON.parse(parsed[SESSION_COOKIE_NAME]);
+    const parsedCookies = parse(sessionCookie);
+    const token = parsedCookies[SESSION_COOKIE_NAME];
+    const decoded = jwt.verify(token, SESSION_SECRET) as JwtPayload;
+    const userSession: UserSession = {
+      userId: decoded.userId as string,
+      lightOrDarkMode: decoded.lightOrDarkMode as string,
+      theme: decoded.theme as string,
+    };
+    return userSession;
   } catch (e: any) {
-    // console.error("Error parsing session cookie", e.message);
+    console.log({
+      sessionCookie,
+      SESSION_SECRET,
+    });
+    console.error("[session] error: " + e.message);
+    console.error("[session] stack: " + e.stack);
     return null;
   }
 }
@@ -42,12 +56,20 @@ export function getUserInfo(): UserSession {
 
 export function setUserSession(userSession: UserSession) {
   const cookieStore = cookies();
-  const serializedSession = serialize(SESSION_COOKIE_NAME, JSON.stringify(userSession), {
-    httpOnly: true,
+  const token = jwt.sign(userSession, SESSION_SECRET, { expiresIn: "30d" });
+  const serializedSession = serialize(SESSION_COOKIE_NAME, token, {
     secure: process.env.NODE_ENV === "production",
     sameSite: "lax",
-    maxAge: 60 * 60 * 24 * 30,
     path: "/",
+    maxAge: 60 * 60 * 24 * 30, // 30 days
+    httpOnly: true,
   });
   cookieStore.set(SESSION_COOKIE_NAME, serializedSession);
 }
+
+// export function resetUserSession() {
+//   const cookieStore = cookies();
+//   const serializedSession = serialize(SESSION_COOKIE_NAME, "", { maxAge: -1, path: "/" });
+//   cookieStore.set(SESSION_COOKIE_NAME, serializedSession);
+//   return redirect("/login");
+// }
