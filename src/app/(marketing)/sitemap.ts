8cc8843c523af7ca54f79ebdef191b8919ed6f31
AlexandroMtzG <alex.martinez@absys.com.mx>
2024-09-28 17:38:34 -0600

    set sitemap pages

--- src/app/(marketing)/sitemap.ts
+++ src/app/(marketing)/sitemap.ts
@@ -1,10 +1,20 @@
 import { getBaseURL } from "@/lib/services/url.server";
+import { defaultAppConfiguration } from "@/modules/core/data/defaultAppConfiguration";
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const links = [
     { url: getBaseURL(), lastModified: new Date() },
     { url: `${getBaseURL()}/contact`, lastModified: new Date() },
+    { url: `${getBaseURL()}/newsletter`, lastModified: new Date() },
+    { url: `${getBaseURL()}/pricing`, lastModified: new Date() },
+    { url: `${getBaseURL()}/terms-and-conditions`, lastModified: new Date() },
+    { url: `${getBaseURL()}/privacy-policy`, lastModified: new Date() },
+    { url: `${getBaseURL()}/rockstack-vs-saasrock`, lastModified: new Date() },
+    { url: `${getBaseURL()}/brand`, lastModified: new Date() },
   ];
+  if (defaultAppConfiguration.affiliates?.provider.rewardfulApiKey) {
+    links.push({ url: `${getBaseURL()}/affiliate-program`, lastModified: new Date() });
+  }
   return links;
 }
